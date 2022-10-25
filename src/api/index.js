af9a24255fb158b675917d34c04e61a3ddc929d5
DevYadav-RaRa <dev.yadav@rara.delivery>
2022-10-25 11:32:32 +0530

    Working fine with remote server

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-var socket = new WebSocket('ws://localhost:9000/ws')
+var socket = new WebSocket('wss://golang-chat-app-backend.herokuapp.com/ws')
 
 let connect = (cb) => {
     console.log("Connecting...");
